b12189d87ebff6a29bfdcada99a72eb1644e2066
Liam Rydén skola <liamr.ryden@gmail.com>
2022-11-21 10:04:10 +0100

    i went sicko mode

--- script.js
+++
@@ -1,9 +0,0 @@
-let cart = document.querySelector('.cart-items-container');
-
-document.querySelector('#cart_btn').onclick = () =>{
-    cart.classList.add('activate');
-}
-
-document.querySelector('#close-form').onclick = () =>{
-    cart.classList.remove('activate');
-}
